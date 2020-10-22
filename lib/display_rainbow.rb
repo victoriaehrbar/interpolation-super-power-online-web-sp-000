26ac392a5cca68c2a7203e1b3bb372c39595c3a5
victoriaehrbar <victoriaehrbar@gmail.com>
2020-10-22 03:27:25 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,2 +1,2 @@
-def display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
-end
+def display_rainbow = (['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+  end
